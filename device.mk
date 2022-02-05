9e7288bef3309d96152a4b25c614bf7220571f37
HuaLiMao-AQ <hualimao_aq@163.com>
2022-02-05 21:54:33 +0800

    atom:device.mk:添加众多基础包 Signed-off-by: HuaLiMao-AQ <hualimao_aq@163.com>

--- device.mk
+++ device.mk
@@ -53,10 +53,39 @@ PRODUCT_PACKAGES += \
     libvisualizer \
     libtinycompress \
 
-    
+# Bluetooth
+PRODUCT_PACKAGES += \
+    btremoted \
+    libldacBT_dec \
+    libbt-vendor \
+    libbluetooth_mtk
+
+# Display
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.composer@2.1-service \
+    android.hardware.graphics.mapper@2.0-impl-2.1 \
+    android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service \
+    libvulkan
+    disable_configstore
+
+# Fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd
+
 # Fstab
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.mt6873:$(TARGET_COPY_OUT_RAMDISK)/fstab.mt6873
+
+# HIDL
+PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.manager@1.0 \
+    libhidltransport \
+    libhwbinder \
     
 # Ramdisk
 PRODUCT_PACKAGES += \
